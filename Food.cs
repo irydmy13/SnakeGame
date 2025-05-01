b9a79a6c17de66cdf63d7f57ba501e9ba6181063
irydmy13 <kotiukir@gmail.com>
2025-05-01 18:08:18 +0300

    генерация еды

--- Food.cs
+++ Food.cs
@@ -1,8 +1,20 @@
-﻿namespace SnakeGame
+﻿using System;
+
+class Food
 {
-    public class Food
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    public void Spawn()
     {
-        public int X { get; set; }
-        public int Y { get; set; }
+        Random rnd = new Random();
+        X = rnd.Next(1, Console.WindowWidth - 2);
+        Y = rnd.Next(1, Console.WindowHeight - 2);
+
+        Console.SetCursorPosition(X, Y);
+        Console.Write("*");
+
+        // Звук при появлении еды
+        SoundPlayerHelper.PlayFoodSound();
     }
 }
